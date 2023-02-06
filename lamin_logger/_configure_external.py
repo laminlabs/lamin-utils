9ba903841ee32d979cfbf6593f27f94b7895756d
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-02-06 14:40:41 +0800

    :art: simplify

--- lamin_logger/_configure_external.py
+++ lamin_logger/_configure_external.py
@@ -3,8 +3,7 @@ import logging
 try:
     import boto3
 
-    boto3.set_stream_logger("boto3", logging.WARNING)
-    boto3.set_stream_logger("botocore", logging.WARNING)
+    # this gets logged 6 times
     boto3.set_stream_logger(name="botocore.credentials", level=logging.WARNING)
     # the 7th logging message of credentials came from aiobotocore
     boto3.set_stream_logger(name="aiobotocore.credentials", level=logging.WARNING)
