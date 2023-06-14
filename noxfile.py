3e810d3ea75b92e6ab4bbfa72e12c7eddbfd8acf
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-14 19:29:36 +0200

    :green_heart: Fix

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,4 @@
-from pathlib import Path
-
+import laminci
 import nox
 
 nox.options.default_venv_backend = "none"
@@ -7,22 +6,11 @@ nox.options.default_venv_backend = "none"
 
 @nox.session
 def lint(session: nox.Session) -> None:
-    session.install("pre-commit")
-    session.run("pre-commit", "install")
-    session.run("pre-commit", "run", "--all-files")
+    laminci.nox.run_pre_commit(session)
 
 
 @nox.session
 def build(session):
     session.install(".[dev,test]")
-    session.run(
-        "pytest",
-        "-s",
-        "--cov=lamin_logger",
-        "--cov-append",
-        "--cov-report=term-missing",
-    )
-    session.run("coverage", "xml")
-    prefix = "." if Path("./lndocs").exists() else ".."
-    session.install(f"{prefix}/lndocs")
-    session.run("lndocs")
+    laminci.nox.run_pytest(session)
+    laminci.nox.build_docs(session)
