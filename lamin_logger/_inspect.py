cf181eadba3957e8b9271f3bac7587a51e35a232
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-06-22 13:20:05 +0200

    🧪 Test empty df

--- lamin_logger/_inspect.py
+++ lamin_logger/_inspect.py
@@ -32,6 +32,20 @@ def inspect(
     """
     import pandas as pd
 
+    def unique_rm_empty(idx: pd.Index):
+        idx = idx.unique()
+        return idx[(idx != "") & (~idx.isnull())]
+
+    # empty DataFrame
+    if df.shape[0] == 0:
+        if return_df:
+            return pd.DataFrame(index=identifiers, data={"__mapped__": False})
+        else:
+            return {
+                "mapped": [],
+                "not_mapped": unique_rm_empty(pd.Index(identifiers)).tolist(),
+            }
+
     # check if index is compliant
     mapped_df = check_if_ids_in_field_values(
         identifiers=identifiers,
@@ -49,10 +63,6 @@ def inspect(
                 "Detected inconsistent casing of mapped terms!\n   For best practice,"
                 " standardize casing via '.map_synonyms()'"
             )
-
-    def unique_rm_empty(idx: pd.Index):
-        idx = idx.unique()
-        return idx[(idx != "") & (~idx.isnull())]
 
     mapped = unique_rm_empty(mapped_df.index[mapped_df["__mapped__"]]).tolist()
     unmapped = unique_rm_empty(mapped_df.index[~mapped_df["__mapped__"]]).tolist()
